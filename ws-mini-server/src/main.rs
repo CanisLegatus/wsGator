2ee77026f53a649712e6ec8b1abbb7d44319ea04
max <maxpershin@yahoo.com>
2025-05-02 18:39:21 +0300

    Added simple WebSocket server with Ping-Pong tech

--- ws-mini-server/src/main.rs
+++ ws-mini-server/src/main.rs
@@ -1,3 +1,136 @@
-fn main() {
-    println!("Hello, world!");
+use futures::SinkExt;
+use futures::StreamExt;
+use std::time::Duration;
+use tokio::net::TcpListener;
+use tokio::sync::mpsc;
+use tokio::sync::watch;
+use tokio_tungstenite::{accept_async, tungstenite::Message, WebSocketStream};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("127.0.0.1:9001").await?;
+
+    println!("🚀 Server listening on ws://127.0.0.1:9001");
+
+    while let Ok((stream, _)) = listener.accept().await {
+        let ws_stream = accept_async(stream)
+            .await
+            .expect("WebSocker connection Error");
+        println!("New connection found!");
+        tokio::spawn(handle_ws(ws_stream));
+    }
+
+    Ok(())
+}
+
+async fn handle_ws(ws_stream: WebSocketStream<tokio::net::TcpStream>) {
+    let (mut write, mut read) = ws_stream.split();
+    let (msg_tx, mut msg_rx) = mpsc::channel::<Message>(1);
+    let (pong_tx, mut pong_rx) = mpsc::channel::<bool>(1);
+    let (shutdown_tx, mut shutdown_rx) = watch::channel(false);
+
+    // Message writer task
+    let writer_task = tokio::spawn({
+        let mut shutdown_rx = shutdown_rx.clone();
+        async move {
+            loop {
+                tokio::select!(
+                    Some(msg) = msg_rx.recv() => {
+                        write.send(msg).await.expect("Can't send a message");
+                    },
+                    _ = shutdown_rx.changed() => {
+                        break;
+                    }
+                )
+            }
+        }
+    });
+
+    // Message sender task
+    let reciever_task = tokio::spawn({
+        let msg_tx = msg_tx.clone();
+        let shutdown_tx = shutdown_tx.clone();
+
+        async move {
+            while let Some(message) = read.next().await {
+                match message {
+                    Ok(Message::Text(txt)) => {
+                        println!("Message recieved: {}", txt);
+                        msg_tx
+                            .send(Message::Text(txt))
+                            .await
+                            .expect("Can't use channel");
+                    }
+                    Ok(Message::Ping(data)) => {
+                        println!("Ping recieved!");
+                        msg_tx
+                            .send(Message::Pong(data))
+                            .await
+                            .expect("Can't use channel");
+                    }
+
+                    Ok(Message::Pong(_data)) => {
+                        println!("Pong Recieved!");
+                        pong_tx.send(true).await.expect("Can't use channel");
+                    }
+
+                    Ok(Message::Close(_)) => {
+                        println!("Client closed connection");
+                        let _ = shutdown_tx.send(true);
+                        break;
+                    }
+                    _ => {}
+                }
+            }
+        }
+    });
+
+    let ping_task = tokio::spawn({
+        let msg_tx = msg_tx.clone();
+        let shutdown_tx = shutdown_tx.clone();
+
+        async move {
+            let mut inteval = tokio::time::interval(Duration::from_secs(30));
+            loop {
+                tokio::select! (
+
+                _ = inteval.tick() => {
+                    println!("Sending ping!");
+                    msg_tx
+                        .send(Message::Ping("ping".into()))
+                        .await
+                        .expect("Can't use channel");
+
+                    let pong_timer =
+                        tokio::time::timeout(Duration::from_secs(10), pong_rx.recv()).await;
+
+                    match pong_timer {
+                        Ok(_) => {
+                            continue;
+                        }
+
+                        _ => {
+                            let _ = shutdown_tx.send(true);
+                            break;
+                        }
+                }},
+
+                _ = shutdown_rx.changed() => {
+                    break;
+                }
+
+
+                )
+            }
+        }
+    });
+
+    tokio::select!(
+        _ = ping_task => {},
+        _ = reciever_task => {},
+        _ = writer_task => {},
+    );
+
+    let _ = shutdown_tx.send(true);
+    println!("Ok... another connection closed!");
 }
