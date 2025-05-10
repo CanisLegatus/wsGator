41fede19b2575d3fa6b2c8238183cd6a10d53265
max <maxpershin@yahoo.com>
2025-05-10 13:59:36 +0300

    Added flood strategy. Some minor fixes

--- wsgator/src/main.rs
+++ wsgator/src/main.rs
@@ -9,14 +9,12 @@ struct Args {
     #[clap(short, long, default_value = "ws://localhost:9001")]
     url: String,
 
+    #[clap(short, long, value_enum, default_value_t = AttackStrategyType::Flat)]
+    strategy: AttackStrategyType,
+
+    // Connections overalls
     #[arg(long, default_value = "100", value_parser = clap::value_parser!(u32).range(1..))]
     connections: u32,
-
-    #[clap(long, default_value = "1", value_parser = clap::value_parser!(u32).range(1..))]
-    waves_amount: u32,
-
-    #[arg(short, long, default_value = "0", value_parser = clap::value_parser!(u32).range(0..))]
-    waves_pause: u32,
 
     #[clap(long, default_value = "30", value_parser = clap::value_parser!(u32).range(1..))]
     connection_duration: u32,
@@ -24,14 +22,23 @@ struct Args {
     #[clap(short, long, default_value = "20", value_parser = clap::value_parser!(u32).range(0..))]
     connection_pause: u32,
 
-    #[clap(short, long, value_enum, default_value_t = AttackStrategyType::Flat)]
-    strategy: AttackStrategyType,
+    // Waves overall
+    #[clap(long, default_value = "1", value_parser = clap::value_parser!(u32).range(1..))]
+    waves_amount: u32,
+
+    #[arg(short, long, default_value = "0", value_parser = clap::value_parser!(u32).range(0..))]
+    waves_pause: u32,
+
+    // Spam amount
+    #[arg(long, default_value = "100", value_parser = clap::value_parser!(u32).range(1..))]
+    spam_pause: u32,
 }
 
 fn get_strategy(strategy_type: AttackStrategyType) -> Arc<dyn AttackStrategy> {
     match strategy_type {
         AttackStrategyType::Flat => Arc::new(FlatStrategy),
         AttackStrategyType::RampUp => Arc::new(RampUpStrategy),
+        AttackStrategyType::Flood => Arc::new(FloodStrategy),
     }
 }
 
--- wsgator/src/strategies.rs
+++ wsgator/src/strategies.rs
@@ -1,10 +1,10 @@
 use crate::Args;
 use async_trait::async_trait;
-use tokio::sync::watch;
 use futures::SinkExt;
 use futures::StreamExt;
 use std::sync::Arc;
 use std::time::Duration;
+use tokio::sync::watch;
 use tokio::time::interval;
 use tokio_tungstenite::{connect_async, tungstenite::Message};
 
@@ -66,7 +66,81 @@ impl AttackStrategy for FloodStrategy {
     }
 
     async fn run(self: Arc<Self>, args: &Args) {
-        
+        for _wave in 0..args.waves_amount {
+            let (tx, rx) = watch::channel(false);
+            let mut interval = interval(Duration::from_secs(args.connection_duration as u64));
+            interval.tick().await;
+
+            // Creating independent timer
+            let timer = args.connection_duration;
+
+            //Spawning timer to cancell all threads
+            tokio::spawn(async move {
+                tokio::time::sleep(Duration::from_secs(timer as u64)).await;
+                let _ = tx.send(true);
+            });
+
+            // Creating connections
+            for i in 0..args.connections {
+                let url = args.url.clone();
+                let strat = Arc::clone(&self);
+                let mut rx = rx.clone();
+                let spam_pause = args.spam_pause;
+
+                // Spawning thread for each connection
+                tokio::spawn(async move {
+                    match connect_async(&url).await {
+                        Ok((mut ws, _)) => {
+                            if let Err(e) = ws
+                                .send(Message::Text(format!("Peer {} saying Hello!", i).into()))
+                                .await
+                            {
+                                println!("Connection {}: Can't send HELLO on connection: {}", i, e);
+                                return;
+                            }
+
+                            // Main strategy logic loop
+                            loop {
+                                tokio::select! {
+                                    msg = ws.next() => {
+                                        let (proceed, message) = strat.handle_messages(msg, i);
+
+                                        if let Some(message) = message {
+                                            if let Err(e) = ws.send(message).await {
+                                                println!("Connection: {}: Can't send message: {}", i, e);
+                                                break;
+                                            }
+                                        }
+
+                                        if !proceed {
+                                            break;
+                                        }
+                                    },
+
+                                    // Spamming with constant messages
+                                    _ = tokio::time::sleep(Duration::from_millis(spam_pause as u64)) => {
+                                       if let Err(e) = ws.send(Message::Text(format!("SPAM! From connection {}", i).into())).await {
+                                           println!("Connection {}: Can't send SPAM message! Error: {}", i, e);
+                                           break;
+                                       }
+                                    }
+
+                                    _ = rx.changed() => {
+                                        // Doing "dirty" connection drop
+                                        println!("Connection {}: Reached its target time. Dropping connection", i);
+                                        drop(ws);
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                        Err(e) => {
+                            println!("Connection {}: Error on WS connection: {}", i, e);
+                        }
+                    }
+                });
+            }
+        }
     }
 }
 
@@ -77,26 +151,26 @@ impl AttackStrategy for FlatStrategy {
     }
     async fn run(self: Arc<Self>, args: &Args) {
         for _wave in 0..args.waves_amount {
-            
             let (tx, rx) = watch::channel(false);
             let mut interval = interval(Duration::from_secs(args.connection_duration as u64));
             interval.tick().await;
-            
+
             // Creating independent timer
-            
             let timer = args.connection_duration;
 
+            // Spawning timer to cancell all threads
             tokio::spawn(async move {
                 tokio::time::sleep(Duration::from_secs(timer as u64)).await;
                 let _ = tx.send(true);
             });
 
-
+            // Creating connetctions
             for i in 0..args.connections {
                 let url = args.url.clone();
                 let strat = Arc::clone(&self);
                 let mut rx = rx.clone();
 
+                // Spawning threads for each connection
                 tokio::spawn(async move {
                     match connect_async(&url).await {
                         Ok((mut ws, _)) => {
@@ -163,7 +237,7 @@ impl AttackStrategy for RampUpStrategy {
 
                 let mut interval = interval(Duration::from_secs(args.connection_duration as u64));
                 interval.tick().await;
-                
+
                 tokio::spawn(async move {
                     match connect_async(&url).await {
                         Ok((mut ws, _)) => {
